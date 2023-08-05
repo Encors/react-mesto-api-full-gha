5f2929876ec91c56447b69955c46ccfdc03a19f2
Encors <cheperish@mail.ru>
2023-08-05 18:08:20 +0500

    xxx

---
+++ frontend/precache-manifest.5329fe5059daa1f45b25edc2c1773344.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8bd62575762731f26fc",
+    "url": "/static/css/main.1ec3e98e.chunk.css"
+  },
+  {
+    "revision": "b8bd62575762731f26fc",
+    "url": "/static/js/main.b8bd6257.chunk.js"
+  },
+  {
+    "revision": "efef7f6fa880e78d2a4c",
+    "url": "/static/js/1.efef7f6f.chunk.js"
+  },
+  {
+    "revision": "5d56232f9799d27b8275",
+    "url": "/static/js/2.5d56232f.chunk.js"
+  },
+  {
+    "revision": "237a870dd2773df21597",
+    "url": "/static/js/runtime~main.237a870d.js"
+  },
+  {
+    "revision": "dc131113894217b5031000575d9de002",
+    "url": "/static/media/Inter-Regular.dc131113.woff2"
+  },
+  {
+    "revision": "661569afe57a38e1529a775a465da20b",
+    "url": "/static/media/Inter-Black.661569af.woff2"
+  },
+  {
+    "revision": "6bfdb35467f51d3ba1f70f8d4a9c2b59",
+    "url": "/static/media/EditButton.6bfdb354.svg"
+  },
+  {
+    "revision": "b2fce4ac45f3d116cd48ec8d068ab723",
+    "url": "/static/media/AddButton.b2fce4ac.svg"
+  },
+  {
+    "revision": "3ac83020fe53b617b79b5e2ad66764af",
+    "url": "/static/media/Inter-Regular.3ac83020.woff"
+  },
+  {
+    "revision": "3d813f810a41032c140568c878fd7d85",
+    "url": "/static/media/heart_black.3d813f81.svg"
+  },
+  {
+    "revision": "d0b121f3a9d3d88afdfd6902d31ee9a0",
+    "url": "/static/media/Inter-Black.d0b121f3.woff"
+  },
+  {
+    "revision": "cb0f2bbfd915a7bfa586c3a3bd2efc48",
+    "url": "/static/media/CloseIcon.cb0f2bbf.svg"
+  },
+  {
+    "revision": "4831282e649d143ce022040beb49ad4f",
+    "url": "/static/media/Trash.4831282e.svg"
+  },
+  {
+    "revision": "75db5319e7e87c587019a5df08d7272c",
+    "url": "/static/media/Inter-Medium.75db5319.woff2"
+  },
+  {
+    "revision": "c0638bea87a05fdfa2bb3bba2efe54e4",
+    "url": "/static/media/Inter-Medium.c0638bea.woff"
+  },
+  {
+    "revision": "68fe0213bb2792e2c581fbd7b72f9969",
+    "url": "/static/media/like.68fe0213.svg"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/static/media/edit_avatar.d476a8b8.svg"
+  },
+  {
+    "revision": "7baba3c1428ee943536a8022d42f0985",
+    "url": "/static/media/logo.7baba3c1.svg"
+  },
+  {
+    "revision": "e38b395c190826b3bf6d78baff1415a0",
+    "url": "/static/media/register_ok.e38b395c.svg"
+  },
+  {
+    "revision": "4072126c596bd9485c6e9601f6d4d35d",
+    "url": "/static/media/not_register.4072126c.svg"
+  },
+  {
+    "revision": "9ce2defaed726f7e1786ecc617d426e1",
+    "url": "/index.html"
+  }
+];
--- frontend/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copyright">&copy; {new Date().getFullYear()}. Акаева Диана</p>
-    </footer>
-  );
-}
